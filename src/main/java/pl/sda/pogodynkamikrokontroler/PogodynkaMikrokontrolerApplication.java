c8fe4b94505f2b07f5eeb378d200ac73c31005e8
Stanisław Serwin <[stanserw@wp.pl]>
2019-05-11 11:31:12 +0200

    initial commit pogodynka-mikrokontroler

---
+++ src/main/java/pl/sda/pogodynkamikrokontroler/PogodynkaMikrokontrolerApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.pogodynkamikrokontroler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogodynkaMikrokontrolerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PogodynkaMikrokontrolerApplication.class, args);
+    }
+
+}
