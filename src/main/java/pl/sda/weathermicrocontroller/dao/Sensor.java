07309962452f9e8b6c5cef1e18ad006590b880ad
Stanisław Serwin <[stanserw@wp.pl]>
2019-05-19 23:28:51 +0200

    refactor Sensor class

--- src/main/java/pl/sda/weathermicrocontroller/dao/Sensor.java
+++ src/main/java/pl/sda/weathermicrocontroller/dao/Sensor.java
@@ -1,36 +1,32 @@
 package pl.sda.weathermicrocontroller.dao;
 
-import org.springframework.data.annotation.Id;
-
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
 /**
  * 1. tworzymy klasę Sensor, która zawiera pola:
- * "id"- unikalny numer identyfikacyjny urządzenia,
+ * "idSensor"- unikalny numer identyfikacyjny urządzenia,
  * "List<SensorData> sensorData" - lista zawierająca odczyty wartości,
  */
 public class Sensor implements Sensorable {
 
-    @Id
-    private String id;
+    private String idSensor;
     private List<SensorData> sensorData;
 
     public Sensor() {
     }
 
-    public Sensor(String id, List<SensorData> sensorData) {
-        this.id = id;
+    public Sensor(String idSensor, List<SensorData> sensorData) {
+        this.idSensor = idSensor;
         this.sensorData = sensorData;
     }
 
-    public String getId() {
-        return id;
+    public String getIdSensor() {
+        return idSensor;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setIdSensor(String idSensor) {
+        this.idSensor = idSensor;
     }
 
     public List<SensorData> getSensorData() {
@@ -46,13 +42,13 @@ public class Sensor implements Sensorable {
         if (this == o) return true;
         if (!(o instanceof Sensor)) return false;
         Sensor sensor = (Sensor) o;
-        return getId().equals(sensor.getId()) &&
+        return getIdSensor().equals(sensor.getIdSensor()) &&
                 getSensorData().equals(sensor.getSensorData());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getSensorData());
+        return Objects.hash(getIdSensor(), getSensorData());
     }
 
     @Override
