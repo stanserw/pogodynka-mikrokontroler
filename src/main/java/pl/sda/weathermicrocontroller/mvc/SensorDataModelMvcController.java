4c3a419efc2495ad8bff59d3be4307d40b409deb
Stanisław Serwin <[stanserw@wp.pl]>
2019-05-31 15:11:36 +0200

    Pagination and Sorting v1

--- src/main/java/pl/sda/weathermicrocontroller/mvc/SensorDataModelMvcController.java
+++ src/main/java/pl/sda/weathermicrocontroller/mvc/SensorDataModelMvcController.java
@@ -1,6 +1,7 @@
 package pl.sda.weathermicrocontroller.mvc;
 
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.weathermicrocontroller.model.SensorDataModel;
 import pl.sda.weathermicrocontroller.repository.SensorDataModelRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RequestMapping(SensorDataModelMvcController.APP_SENSOR_PATH)
@@ -32,6 +34,26 @@ public class SensorDataModelMvcController {
         listViewModel.addAttribute("allSensors", sensorDataList);
 
         return "list-sensors-readings";
+    }
+
+    @GetMapping("/pagelist")
+    public String allSensorsPageView(HttpServletRequest request, Model model) {
+
+        int page = 0; //default page number is 0 (yes it is weird)
+        int size = 10; //default page size is 10
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
+        model.addAttribute("allSensors", sensorDataModelRepository.findAll(PageRequest.of(page, size,
+                Sort.by("date").descending())));
+
+        return "list-sensors-readings-page-ten-links";
     }
 
 }
--- src/main/java/pl/sda/weathermicrocontroller/repository/SensorDataModelRepository.java
+++ src/main/java/pl/sda/weathermicrocontroller/repository/SensorDataModelRepository.java
@@ -1,10 +1,10 @@
 package pl.sda.weathermicrocontroller.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pl.sda.weathermicrocontroller.model.SensorDataModel;
 
 @Repository
 public interface SensorDataModelRepository extends MongoRepository<SensorDataModel, String> {
-
 }
